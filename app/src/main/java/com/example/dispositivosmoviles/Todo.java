47dca23ed45e5ab5fcdc64b68112cd4fdd951388
¨BrianDorantes¨ <brian_dorantes@icloud.com>
2023-05-11 23:58:07 -0600

    se crea class Todo

---
+++ app/src/main/java/com/example/dispositivosmoviles/Todo.java
@@ -0,0 +1,35 @@
+package com.example.dispositivosmoviles;
+
+
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Application;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.identity.BeginSignInRequest;
+import com.google.android.gms.auth.api.identity.SignInClient;
+import com.google.android.gms.auth.api.identity.SignInCredential;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+public class Todo extends AppCompatActivity {
+
+
+}
+
+
+
+
